5e6a413d6f76882aeda4fbf9d3644962fe871a41
Vaibhav Matere <vpm00718897@gmail.com>
2023-01-12 16:23:10 +0530

    function for multiple arguments

--- function.js
+++ function.js
@@ -6,3 +6,21 @@ function addTwo(num) {
     return num + 2;
 }
 addTwo("5");
+// to avoid all of this type checking and input validations,
+function addTwoTo(num) {
+    // num.toLowerCase();
+    // will give an error => Property 'toLowerCase' does not exist on type 'number'
+    return num + 2;
+}
+// addTwoTo("5")
+// will give an error => 'string' is not assignable to parameter of type 'number'
+addTwoTo(5);
+// FUNCTION - for multiple arguments
+function signUpUser(name, email, feesPaid) {
+}
+signUpUser("Vaibhav", "vpm00718897@gmail.com", false);
+// FUNCTION - for multiple arguments - default type
+function loginUser(name, email, feesPaid) {
+    if (feesPaid === void 0) { feesPaid = false; }
+}
+loginUser("Vaibhav", "vpm18897@gmail.com");
--- function.ts
+++ function.ts
@@ -18,4 +18,18 @@ function addTwoTo(num: number) {
 // will give an error => 'string' is not assignable to parameter of type 'number'
 addTwoTo(5);
 
+// FUNCTION - for multiple arguments
+
+function signUpUser(name:string, email:string, feesPaid:boolean) {
+}
+
+signUpUser("Vaibhav", "vpm00718897@gmail.com", false);
+
+// FUNCTION - for multiple arguments - default type
+
+function loginUser(name:string, email:string, feesPaid = false) {
+}
+
+loginUser("Vaibhav", "vpm18897@gmail.com");
+
 export{}
