afe0db9fcffcaee181fc927a72a21ece06fbe205
Vaibhav Matere <vpm00718897@gmail.com>
2023-01-17 13:47:36 +0530

    tuple types - combination of str and array

---
+++ tuple.js
@@ -0,0 +1,8 @@
+var stringNumberTuple;
+stringNumberTuple = ["vaibhav", 25];
+// pass the initial two conditions of string and number.
+var strNumTuple;
+// strNumTuple = ["Shubham", 23, 12, "Astitva Group"];
+// now, this will give an error as -
+// "Type '[string, number, number, string]' is not assignable to type '[string, number]'.
+// Source has 4 element(s) but target allows only 2."
