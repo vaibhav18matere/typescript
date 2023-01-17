fd2c5b68649015c39d02c171c195747711e8da53
Vaibhav Matere <vpm00718897@gmail.com>
2023-01-17 13:38:23 +0530

    array types

---
+++ array.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var strArray;
+var numArray;
+var boolArray;
+strArray = ["Vaibhav", "Matere", "Developer"];
+numArray = [1, 2, 3, 4, 5];
+boolArray = [true, false, true, true];
+console.log(strArray);
+console.log(numArray);
+console.log(boolArray);
+// other way to define array types is -
+var stringArray;
+stringArray = ["Shubham", "Matere", "Entrepreneur"];
+var numberArray;
+numberArray = [6, 7, 8, 9, 10];
+var booleanArray;
+booleanArray = [true, true, true, false];
---
+++ array.ts
@@ -0,0 +1,25 @@
+let strArray: string[];
+let numArray: number[];
+let boolArray: boolean[];
+
+strArray = ["Vaibhav", "Matere", "Developer"];
+numArray = [1, 2, 3, 4, 5];
+boolArray = [true, false, true, true];
+
+console.log(strArray);
+console.log(numArray);
+console.log(boolArray);
+
+// other way to define array types is -
+
+let stringArray: Array<string>;
+stringArray = ["Shubham", "Matere", "Entrepreneur"];
+
+let numberArray: Array<number>;
+numberArray = [6, 7, 8, 9, 10];
+
+let booleanArray: Array<boolean>;
+booleanArray = [true, true, true, false];
+
+
+export {};
