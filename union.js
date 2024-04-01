1d9e7163a9842f94d46ace98ec3754401369763b
Vaibhav Matere <vpm00718897@gmail.com>
2024-04-01 16:53:31 +0530

    union - func & data type in array

--- union.js
+++ union.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // when hitesh is user
 var hitesh = {
     name: "vai",
@@ -9,3 +11,19 @@ var hiteshAdm = {
     userName: "Admin Bhai",
     age: 156,
 };
+// UNION IN CASE OF FUNCTIONS
+function getDbId(id) {
+    // id.toLowerCase();
+    // this will give an error => Property 'toLowerCase' does not exist on type 'string | number'.
+    // so, we have to first verify before using it
+    if (typeof id === 'string') {
+        id.toLowerCase();
+    }
+    console.log("The databse id is ".concat(id));
+}
+// now, we can pass string and number as an argument for a function
+console.log(getDbId(1));
+console.log(getDbId("2"));
+var data = [1, 2, 3, 4, 5, "start"];
+// Type 'string' is not assignable to type 'number'.
+var names = ["vai", "bhav", 123];
--- union.ts
+++ union.ts
@@ -22,3 +22,34 @@ let hiteshAdm: User | Admin = {
      userName : "Admin Bhai",
      age: 156,
 }
+
+// UNION IN CASE OF FUNCTIONS
+
+function getDbId(id: number | string) {
+     // id.toLowerCase();
+     // this will give an error => Property 'toLowerCase' does not exist on type 'string | number'.
+     // so, we have to first verify before using it
+     if (typeof id === 'string') {
+          id.toLowerCase();
+     } 
+     console.log(`The databse id is ${id}`);
+}
+
+// now, we can pass string and number as an argument for a function
+
+
+console.log(getDbId(1));
+console.log(getDbId("2"));
+
+
+const data: number[] = [1, 2, 3, 4, 5, "start"];
+// Type 'string' is not assignable to type 'number'.
+
+const names: string[] = ["vai", "bhav", 123];
+// Type 'number' is not assignable to type 'string'.
+
+// if you want to have numbers and string in an array then,
+
+const mixedData : (number|string)[] = [1,2,3, "vaibhav", "matere", 4, 5]
+
+export{}
