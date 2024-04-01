70431f346b40334fa49213841d7a3f226d5e1e35
Vaibhav Matere <vpm00718897@gmail.com>
2024-04-01 13:15:26 +0530

    introduction file rename

--- intro.js
+++ intro.js
@@ -1,7 +1,8 @@
-// var user = { name: "Vaibhav", age: 20 };
-// var email = user.name;
-// var num1 = 3;
-// var num2 = "3";
-// var sum = num1 + num2;
-// console.log(sum);
-// console.log("Vaibhav");
+var user = { name: "Vaibhav", age: 20 };
+var email = user.name;
+var num1 = 3;
+var num2 = "3";
+var sum = num1 + num2;
+console.log(sum);
+console.log("Vaibhav");
+var naav = "Vaibhav";
--- intro.ts
+++ intro.ts
@@ -1,13 +1,15 @@
-// let user = {name:"Vaibhav", age:20}
-//  let email = user.name;
+let user = {name:"Vaibhav", age:20}
+ let email = user.name;
 
-// let num1 = 3;
-// let num2 = "3";
+let num1 = 3;
+let num2 = "3";
 
-// let sum = num1+num2;
+let sum = num1+num2;
 
-// console.log(sum);
+console.log(sum);
 
-// console.log("Vaibhav");
+console.log("Vaibhav");
 
 let naav: string = "Vaibhav";
+
+export{}
