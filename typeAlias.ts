09baf7305e75ccba674b4f9122d543e1b6f8e968
Vaibhav Matere <vpm00718897@gmail.com>
2024-04-01 16:08:06 +0530

    readonly

--- typeAlias.ts
+++ typeAlias.ts
@@ -1,7 +1,7 @@
 type User = {
     name: string,
     phone: number,
-    isActive: false
+    isActive: boolean
 }
 
 function createUser (user:User):User{
@@ -10,4 +10,23 @@ function createUser (user:User):User{
 
 createUser({ name: "Vaibhav", phone: 987542, isActive: false });
 
+// READONLY
+
+type User2 = {
+   readonly id: number,
+    name: string,
+    age: number,
+    isActive: boolean,
+}
+
+let myUSer: User2 = {
+     id: 1,
+     name: "Vai",
+     age: 26,
+     isActive: true,
+}
+
+// if we do,
+// myUSer.id = 12;
+// it will show error => "Cannot assign to 'id' because it is a read-only property."
 export{}
