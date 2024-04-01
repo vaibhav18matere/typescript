996712de5b119aaf02eb9fc72d25044637b971c9
Vaibhav Matere <vpm00718897@gmail.com>
2024-04-01 13:16:12 +0530

    basic variable check

---
+++ variableMe.js
@@ -0,0 +1,2 @@
+var greetings = "Hello There";
+console.log(greetings);
---
+++ variableMe.ts
@@ -0,0 +1,14 @@
+let greetings:string = "Hello There";
+
+// checck by typing  greetings. 
+// and we will get string methods as a suggestion in VS code
+// greetings.
+
+console.log(greetings);
+
+// if we try to do
+// greetings = 5;
+// we will get an error 
+// "Type 'number' is not assignable to type 'string'."
+
+export { }
